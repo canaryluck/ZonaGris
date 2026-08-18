8441ef00872f941084b5638132376b9fbae54be0
canaryluck <gokuwiti@gmail.com>
2026-08-18 23:35:00 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,25 @@
 import os
 import telebot
+from http.server import BaseHTTPRequestHandler
 
 bot = telebot.TeleBot(os.environ["BOT_TOKEN"])
 
-def handler(request):
-    if request.method == "POST":
-        update = telebot.types.Update.de_json(request.body.decode("utf-8"))
-        bot.process_new_updates([update])
-        return "OK"
+class handler(BaseHTTPRequestHandler):
 
-    return "Bot ZonaGris funcionando 👋"
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Bot ZonaGris funcionando")
+
+    def do_POST(self):
+        content_length = int(self.headers.get("Content-Length", 0))
+        body = self.rfile.read(content_length)
+
+        update = telebot.types.Update.de_json(body.decode("utf-8"))
+        bot.process_new_updates([update])
+
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"OK")
